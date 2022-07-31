862fb4940e527cb98460216901835e0f023e5a62
riquiamado <carlosamado221471@gmail.com>
2022-07-31 17:21:20 -0300

    ejercisio 5 terminado

---
+++ ejercisio5.js
@@ -0,0 +1,14 @@
+let altura = 165;
+let alturaMetro = 1.65;
+let peso = 95.5;
+let alturaHaciaArriba = Math.ceil(alturaMetro);
+let pesoHaciaAbajo=   Math.floor(peso);
+let maximoValor = Number.MAX_VALUE + 1;
+
+
+console.log(altura);
+console.log(alturaMetro);
+console.log(peso);
+console.log(alturaHaciaArriba);
+console.log(pesoHaciaAbajo);
+console.log(maximoValor);
